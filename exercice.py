3dc58e64a6e3daae4a60da7d32231a0bdbbd6f53
ThisHobbit <jeremy.sinou@gmail.com>
2022-09-04 03:50:40 -0400

    Update exercice.py
    commit and push test

--- exercice.py
+++ exercice.py
@@ -23,6 +23,7 @@ def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float
 def to_degrees(angle_rads: float) -> tuple:
     return 0.0, 0.0, 0.0
 
+#test
 
 def to_celsius(temperature: float) -> float:
     return 0.0
